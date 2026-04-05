21aaa460b77a7af268f20340f6509ebad937d5d5
Abdllah Abbara <181393678+Qxx05@users.noreply.github.com>
2026-04-05 19:43:08 -0400

    SearchBar done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,26 @@
 {/* Abdllah Abbara - 991798700*/}
+
+import { useState } from "react";
+
+function SearchBar(props) {
+    const [city, setCity] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        props.onSearch(city);
+}
+
+return (
+    <form onSubmit={handleSubmit}>
+        <input
+            type="text"
+            placeholder="Enter city name"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+        />
+        <button type="submit">Search</button>
+    </form>
+);
+}
+
+export default SearchBar;
