2831819485d5366fbce5cb8009fb57286dd7c939
Abdllah Abbara <181393678+Qxx05@users.noreply.github.com>
2026-04-05 19:55:03 -0400

    Added ErrorMessage

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,1 +1,9 @@
 {/* Abdllah Abbara - 991798700*/}
+
+function ErrorMessage(props) {
+    return (
+        <div className="message box error box">
+            <p>{props.message}</p>
+        </div>
+    )
+}
