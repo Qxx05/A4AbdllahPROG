82b3000e2eacaf41c1cdfed94787a9950723be1a
Abdllah Abbara <181393678+Qxx05@users.noreply.github.com>
2026-04-05 19:51:59 -0400

    Added Loading Component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,1 +1,9 @@
 {/* Abdllah Abbara - 991798700*/}
+
+function Loading() {
+    return (
+        <div className="message box">
+            <p>Loading weather data.....</p>
+        </div>
+    )
+}
