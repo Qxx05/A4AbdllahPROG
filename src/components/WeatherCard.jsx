0e2a23da1e37f23c2540cb05344447bfa0f46ed3
Abdllah Abbara <181393678+Qxx05@users.noreply.github.com>
2026-04-05 20:02:18 -0400

    Added WeatherCard component

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,1 +1,27 @@
 {/* Abdllah Abbara - 991798700*/}
+
+function WeatherCard(props) {
+    const weather = props.weather;
+
+    return (
+        <div className="weather card">
+            <h2>{weather.name}</h2>
+
+            <p>
+                <strong>Temperature:</strong> {weather.main.temp} °C
+            </p>
+
+            <p>
+                <strong>Description:</strong> {weather.weather[0].description}
+            </p>
+
+            <p>
+                <strong>Humidity:</strong> {weather.main.humidity}%
+            </p>
+
+            <p>
+                <strong>Wind Speed:</strong> {weather.wind.speed} m/s
+            </p>
+        </div>
+    )
+}
