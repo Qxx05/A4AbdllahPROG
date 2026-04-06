3be0fa7f77364a100488cf3dc5c56a0e9b9ff92e
Abdllah Abbara <181393678+Qxx05@users.noreply.github.com>
2026-04-05 21:04:41 -0400

    Added parts of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,58 @@
 {/* Abdllah Abbara - 991798700*/}
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useState, useEffect } from 'react'
 import './App.css'
+import SearchBar from './components/SearchBar';
+import Loading from './components/Loading';
+import ErrorMessage from './components/ErrorMessage';
+import WeatherCard from './components/WeatherCard';
+import ForecastList from './components/ForecastList';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (<h1>Abdllah Abbara</h1>)
+  const [searchCity, setSearchCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [forecast, setForecast] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const apiKey = "24b30766f970b0b8d551fdda30d5c876";
+
+  function handleSearch(city) {
+    setSearchCity(city);
+  }
+
+  useEffect(function () {
+    
+    if (searchCity === "") {
+      return;
+    }
+
+    setLoading(true);
+    setError("");
+    setWeather(null);
+    setForecast([]);
+
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + apiKey + "&units=metric")
+
+    .then(function(response) {
+      
+      if (!response.ok) {
+        throw new Error("City not found");
+      }
+      
+      return response.json();
+    })
+
+    .then(function (data) {
+      setWeather(data);
+    })
+
+    .catch(function() {
+      setError("Error pulling weather data");
+    });
+
+    // forecast (bonus)
+
+  })
 }
-
 export default App
